92e9fe1a87a9a26214a377f81efb0c4f22ce769e
nickmly <nickmly@gmail.com>
2019-07-14 19:21:28 -0400

    Mobile navbar

--- SocialMediaAngular/ClientApp/src/app/navbar/navbar.component.ts
+++ SocialMediaAngular/ClientApp/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  @ViewChild('navLinks', {read: false, static: false})
+  navLinks: ElementRef;
+
+  @ViewChild('navBurger', {read: false, static: false})
+  navBurger: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    // If window is smaller than breakpoint width, remove navLinks and add navBurger
+    if(event.target.innerWidth <= 800) {
+      this.navBurger.nativeElement.classList.remove('hidden');
+      this.navLinks.nativeElement.classList.add('hidden');
+    }
+    else {
+      // Else add nav links with no burger
+      this.navBurger.nativeElement.classList.add('hidden');
+      this.navLinks.nativeElement.classList.remove('hidden');
+    }
+  }
+
+  toggleNavbar() {
+    this.navLinks.nativeElement.classList.toggle('hidden');
+  }
+
+  onClickNavLink() {
+    this.navLinks.nativeElement.classList.add('hidden');
+  }
 }
