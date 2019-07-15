974cc1c80801f0d34e82c80487f8336ddeb4dcfb
nickmly <nickmly@gmail.com>
2019-07-14 20:02:09 -0400

    Footer added

--- SocialMediaAngular/ClientApp/src/app/app.component.ts
+++ SocialMediaAngular/ClientApp/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SocialMedia';
+
+  @ViewChild('disclaimer', {read: false, static: false})
+  disclaimer: ElementRef;
+  
+  hideDisclaimer() {
+    this.disclaimer.nativeElement.classList.add('hidden');
+  }
 }
